a3ba71fd50c93c0e59b8f6cd572063d31c56b5d5
Maulana Sodiqin <53475078+maulanasdqn@users.noreply.github.com>
2024-02-08 20:49:29 +0700

    chore: update devenv config (#7)

--- devenv.nix
+++ devenv.nix
@@ -1,19 +1,74 @@
 { pkgs, ... }:
 
 {
-  env.GREET = "Welcome To Neo-Siakad Dev Environment!";
+  packages = with pkgs; [
+    bun
+    nodejs
+    (writeScriptBin "helpme" ''
+      __usage="
+      👋 Welcome to NeoSiakad development environment. 🚀
+      If you see this message, it means your are inside the Nix shell ❄️.
 
-  packages = with pkgs; [ bun nodejs ];
+      [Info]===============================================================>
 
-  scripts.hello.exec = "echo $GREET";
+      NodeJS Version: v${nodejs.version}
+      Bun Version: v${bun.version}
+
+      Command available:
+        - start:            start project in production ( need to run build first  ) 🛹
+        - build:            build project for production
+        - dev:              start development server
+        - start-postgres:   start postgres-db
+        - db-gen:           generate drizzle migration
+        - db-push:          push migration
+        - db-seed:          seed database
+        - helpme:           show this messages
+
+      Repository:
+        - https://github.com/maulanasdqn/neo-siakad
+      [Info]===============================================================>
+      "
+      echo "$__usage"
+    '')
+
+    (writeScriptBin "dev" ''
+      bun dev
+    '')
+
+    (writeScriptBin "build" ''
+      bun run build
+    '')
+
+    (writeScriptBin "start" ''
+      bun start
+    '')
+
+    (writeScriptBin "db-generate" ''
+      bun db:gen
+    '')
+
+    (writeScriptBin "db-push" ''
+      bun db:push
+    '')
+
+    (writeScriptBin "db-seed" ''
+      bun db:seed
+    '')
+
+  ];
 
   enterShell = ''
-    hello
+    helpme
   '';
+
+  languages.typescript.enable = true;
 
   dotenv.enable = true;
 
-  languages.nix.enable = true;
+  services.postgres = with pkgs; {
+    enable = true;
+    package = postgresql_15;
+    initialDatabases = [{ name = "neosiakad-db"; }];
+  };
 
-  languages.typescript.enable = true;
 }
--- libs/auth/config.ts
+++ libs/auth/config.ts
@@ -1,6 +1,5 @@
 import { NextAuthConfig } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authConfig = {
   pages: {
@@ -10,19 +9,9 @@ export const authConfig = {
     strategy: "jwt",
   },
   providers: [
-    CredentialsProvider({
-      async authorize(credentials) {
-        return credentials;
-      },
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
-  callbacks: {
-    async session({ session }) {
-      return session;
-    },
-  },
 } satisfies NextAuthConfig;
