057fcbe7c387a784c733844a57e9ecd844e520df
sugiartofahmi <sugiartofahmi@gmail.com>
2024-02-13 06:20:46 +0700

    improvement: move relation schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -5,12 +5,13 @@ import {
   primaryKey,
   integer,
   boolean,
+  serial,
 } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "@auth/core/adapters";
 import { relations } from "drizzle-orm";
 
 export const users = pgTable("users", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   fullname: text("fullname"),
   email: text("email").notNull(),
   password: text("password"),
@@ -21,25 +22,13 @@ export const users = pgTable("users", {
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
 
-export const usersRelations = relations(users, ({ one, many }) => ({
-  userAffiliations: one(userAffiliations),
-  roles: one(roles, {
-    fields: [users.roleId],
-    references: [roles.id],
-  }),
-}));
-
 export const roles = pgTable("roles", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   name: text("name"),
   permissions: text("permissions").array(),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
-
-export const rolesRelations = relations(roles, ({ many }) => ({
-  users: many(users),
-}));
 
 export const userAffiliations = pgTable(
   "user_affiliations",
@@ -62,25 +51,9 @@ export const userAffiliations = pgTable(
     }),
   })
 );
-export const userAffiliationsRelations = relations(
-  userAffiliations,
-  ({ one }) => ({
-    faculty: one(faculty, {
-      fields: [userAffiliations.facultyId],
-      references: [faculty.id],
-    }),
-    department: one(department, {
-      fields: [userAffiliations.departmentId],
-      references: [department.id],
-    }),
-    organization: one(organizations, {
-      fields: [userAffiliations.organizationId],
-      references: [organizations.id],
-    }),
-  })
-);
+
 export const activities = pgTable("activities", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   name: text("name"),
   description: text("description"),
   location: text("location"),
@@ -92,16 +65,8 @@ export const activities = pgTable("activities", {
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
 
-export const activitiesRelations = relations(activities, ({ one, many }) => ({
-  users: one(users, {
-    fields: [activities.applicantId],
-    references: [users.id],
-  }),
-  proposals: many(proposals),
-}));
-
 export const organizations = pgTable("organizations", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   name: text("name"),
   facultyId: text("faculty_id").references(() => faculty.id),
   departmentId: text("department_id").references(() => department.id),
@@ -112,61 +77,29 @@ export const organizations = pgTable("organizations", {
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
 
-export const organizationsRelations = relations(organizations, ({ one }) => ({
-  faculty: one(faculty, {
-    fields: [organizations.facultyId],
-    references: [faculty.id],
-  }),
-  department: one(department, {
-    fields: [organizations.departmentId],
-    references: [department.id],
-  }),
-  organizationLevel: one(organizationLevel, {
-    fields: [organizations.organizationLevelId],
-    references: [organizationLevel.id],
-  }),
-}));
-
 export const organizationLevel = pgTable("organization_level", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   name: text("name"),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
-
-export const organizationLevelRelations = relations(
-  organizationLevel,
-  ({ many }) => ({
-    organization: many(organizations),
-  })
-);
 
 export const faculty = pgTable("faculty", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   name: text("name"),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
-
-export const facultyRelations = relations(faculty, ({ many }) => ({
-  userAffiliations: many(userAffiliations),
-  organizations: many(organizations),
-}));
 
 export const department = pgTable("department", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   name: text("name"),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
 
-export const departmentRelations = relations(department, ({ many }) => ({
-  userAffiliations: many(userAffiliations),
-  organizations: many(organizations),
-}));
-
 export const reviews = pgTable("reviews", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   reviewerId: text("reviewer_id").references(() => users.id),
   proposalId: text("proposal_id").references(() => proposals.id),
   isApproved: boolean("is_approved"),
@@ -175,19 +108,8 @@ export const reviews = pgTable("reviews", {
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
 
-export const reviewsRelations = relations(reviews, ({ one }) => ({
-  reviewer: one(users, {
-    fields: [reviews.reviewerId],
-    references: [users.id],
-  }),
-  proposal: one(proposals, {
-    fields: [reviews.proposalId],
-    references: [proposals.id],
-  }),
-}));
-
 export const proposals = pgTable("proposals", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   title: text("title"),
   file: text("file"),
   proposalStatusId: text("proposal_staus_id").references(
@@ -200,30 +122,12 @@ export const proposals = pgTable("proposals", {
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
 
-export const proposalsRelations = relations(proposals, ({ one }) => ({
-  proposalStatus: one(proposalStatus, {
-    fields: [proposals.proposalStatusId],
-    references: [proposalStatus.id],
-  }),
-  activity: one(activities, {
-    fields: [proposals.activityId],
-    references: [activities.id],
-  }),
-}));
-
 export const proposalStatus = pgTable("proposal_status", {
-  id: text("id").notNull().primaryKey(),
+  id: serial("id").primaryKey(),
   name: text("name"),
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
-
-export const proposalStatusRelations = relations(
-  proposalStatus,
-  ({ many }) => ({
-    proposals: many(proposals),
-  })
-);
 
 export const accounts = pgTable(
   "account",
@@ -266,5 +170,99 @@ export const verificationTokens = pgTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
+  })
+);
+
+//
+export const usersRelations = relations(users, ({ one, many }) => ({
+  userAffiliations: one(userAffiliations),
+  roles: one(roles, {
+    fields: [users.roleId],
+    references: [roles.id],
+  }),
+}));
+
+export const rolesRelations = relations(roles, ({ many }) => ({
+  users: many(users),
+}));
+
+export const userAffiliationsRelations = relations(
+  userAffiliations,
+  ({ one }) => ({
+    faculty: one(faculty, {
+      fields: [userAffiliations.facultyId],
+      references: [faculty.id],
+    }),
+    department: one(department, {
+      fields: [userAffiliations.departmentId],
+      references: [department.id],
+    }),
+    organization: one(organizations, {
+      fields: [userAffiliations.organizationId],
+      references: [organizations.id],
+    }),
+  })
+);
+export const activitiesRelations = relations(activities, ({ one, many }) => ({
+  users: one(users, {
+    fields: [activities.applicantId],
+    references: [users.id],
+  }),
+  proposals: many(proposals),
+}));
+export const organizationsRelations = relations(organizations, ({ one }) => ({
+  faculty: one(faculty, {
+    fields: [organizations.facultyId],
+    references: [faculty.id],
+  }),
+  department: one(department, {
+    fields: [organizations.departmentId],
+    references: [department.id],
+  }),
+  organizationLevel: one(organizationLevel, {
+    fields: [organizations.organizationLevelId],
+    references: [organizationLevel.id],
+  }),
+}));
+export const organizationLevelRelations = relations(
+  organizationLevel,
+  ({ many }) => ({
+    organization: many(organizations),
+  })
+);
+export const facultyRelations = relations(faculty, ({ many }) => ({
+  userAffiliations: many(userAffiliations),
+  organizations: many(organizations),
+}));
+export const departmentRelations = relations(department, ({ many }) => ({
+  userAffiliations: many(userAffiliations),
+  organizations: many(organizations),
+}));
+
+export const reviewsRelations = relations(reviews, ({ one }) => ({
+  reviewer: one(users, {
+    fields: [reviews.reviewerId],
+    references: [users.id],
+  }),
+  proposal: one(proposals, {
+    fields: [reviews.proposalId],
+    references: [proposals.id],
+  }),
+}));
+export const proposalsRelations = relations(proposals, ({ one }) => ({
+  proposalStatus: one(proposalStatus, {
+    fields: [proposals.proposalStatusId],
+    references: [proposalStatus.id],
+  }),
+  activity: one(activities, {
+    fields: [proposals.activityId],
+    references: [activities.id],
+  }),
+}));
+
+export const proposalStatusRelations = relations(
+  proposalStatus,
+  ({ many }) => ({
+    proposals: many(proposals),
   })
 );
--- drizzle/seed.ts
+++ drizzle/seed.ts
@@ -22,6 +22,56 @@ const dbQueryClient = new Pool({
   connectionString: dbUrl,
 });
 
+const db = drizzle(dbQueryClient, {
+  schema: {
+    users,
+    roles,
+    userAffiliations,
+    activities,
+    organizations,
+    organizationLevel,
+    faculty,
+    department,
+    reviews,
+    proposals,
+    proposalStatus,
+    accounts,
+    sessions,
+    verificationTokens,
+  },
+});
+
+const seedRole = async () => {
+  const roleExist = await db.select({ id: roles.id }).from(roles);
+
+  if (roleExist.length) {
+    return;
+  }
+  console.log("Seeding roles... 🚀");
+
+  await db.insert(roles).values([
+    {
+      name: "Admin",
+    },
+    {
+      name: "Wakil Rektor",
+    },
+    {
+      name: "Kemahasiswaan",
+    },
+    {
+      name: "Wakil Dekan",
+    },
+    {
+      name: "Ketua Program Studi",
+    },
+    {
+      name: "Ormawa",
+    },
+  ]);
+  console.log("Seeding roles done! 🎊");
+};
+
 const main = async () => {
   try {
     console.log("seed");
